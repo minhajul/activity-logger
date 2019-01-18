40a3a593dec229c764adcaca5e7081d065ebf03d
minhajul <kmsohelinfo@gmail.com>
2019-01-18 14:16:15 +0600

    Updated

--- src/Commands/DeleteOlderActivity.php
+++ src/Commands/DeleteOlderActivity.php
@@ -2,8 +2,8 @@
 
 namespace Minhajul\ActivityLogger\Commands;
 
-use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
+use Illuminate\Console\Command;
 use Minhajul\ActivityLogger\Models\Activity;
 
 class DeleteOlderActivity extends Command
@@ -31,7 +31,6 @@ class DeleteOlderActivity extends Command
     {
         parent::__construct();
     }
-
 
     public function handle()
     {
